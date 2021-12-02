1a24adfb0dc8e0c30e3a07435dea9ad3fb346ac0
guest1 <akm17122003@gmail.com>
2021-12-02 15:30:02 +0600

    commit2

--- app/src/main/java/kg/kamila/midterm2/MainActivity.kt
+++ app/src/main/java/kg/kamila/midterm2/MainActivity.kt
@@ -1,11 +1,22 @@
 package kg.kamila.midterm2
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val exit = findViewById<ImageView>(R.id.search)
+        exit.setOnClickListener {
+            startActivity(Intent(this,MainActivity2::class.java))
+            finish()
+        }
     }
 }
